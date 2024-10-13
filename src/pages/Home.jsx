7dd6b3e2b1334775d91e7a575756909bac169525
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-10-13 14:53:31 +0200

    some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,13 @@ import SearchBar from "../components/SearchBar";
 import { useSelector } from "react-redux";
 
 export default function HomePage() {
-  const MOVIES_REDUX = useSelector((state) => state.movies_.movie_list);
+  const MOVIES = useSelector((state) => state.movies_.movie_list);
 
   return (
     <main>
       <h1>Last Movies</h1>
       <section>
-        <SearchBar movies={MOVIES_REDUX} />
+        <SearchBar movies={MOVIES} />
       </section>
     </main>
   );

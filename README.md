09911ac8bc195e7ff5559d82b24fecd765914ddb
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-10-17 11:40:03 +0200

    details page responsive to mobile

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # React + Vite
 
-Technologies Used: Vanilla Css/React-Router-Dom /Redux Toolkit / Cypress /Helmet
+Tools Used:
+-Firefox Developer Edition
+-Vs Code
+-Postman
+
+Services Used:
+-Tmdb Movies
+
+Technologies Used:
+-Vanilla Css
+-React-Router-Dom
+-Redux Toolkit
+-Cypress
+-Helmet
+
+Responsive to:
+-Galaxy S20 Ultra Android 11
+-Desktop
--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,29 +3,52 @@ import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import BackBtn from "../../components/common/Button/BackBtn";
 import { BASE_POSTER_URL } from "../../services/api-host";
+import "./Details.css";
 
 export default function DetailsPage() {
   /* const { movieId } = useParams(); */
   const location = useLocation(); // Get location object
   const { movie } = location.state || {}; // Extract movie from state
-  const { id, title, overview, poster_path } = movie;
+  const {
+    id,
+    title,
+    original_title,
+    popularity,
+    overview,
+    vote_average,
+    poster_path,
+  } = movie;
 
   const navigate = useNavigate();
 
   return (
-    <main>
-      <h1>Movie Details</h1>
+    <main className="container">
+      <h1>Details</h1>
       <img
+        className="poster-img"
         src={`${BASE_POSTER_URL}/${poster_path}`}
-        width="500"
-        height="500"
         alt={title}
       />
-      <h2>Details</h2>
-      <p>id :{id}</p>
-      <p>title:{title}</p>
-      <p>Description :{overview}</p>
-      <BackBtn onClickFn={() => navigate("..")} />
+      <section>
+        <p>
+          <strong>Tmdb Id:</strong> {id}
+        </p>
+        <p>
+          <strong>Original Title</strong>: {original_title}
+        </p>
+        <p>
+          <strong>Description</strong>: {overview}
+        </p>
+        <p>
+          <strong>Rating</strong>: {vote_average}
+        </p>
+        <p>
+          <strong>Popularity</strong>: {popularity}
+        </p>
+      </section>
+      <div className="back-btn__container">
+        <BackBtn onClickFn={() => navigate("..")} />
+      </div>
     </main>
   );
 }

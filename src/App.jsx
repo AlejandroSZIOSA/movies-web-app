0ae9af5f90f221db5fa9217b278a38968a0ea64f
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-10-19 03:35:05 +0200

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,15 @@ import HomePage from "./pages/home/Home";
 import DetailsPage from "./pages/details/Details";
 import FavoritesPage from "./pages/favorites/Favorites";
 
+// Define routes for the app, using React Router's createBrowserRouter and RouterProvider components.
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      {
-        /* path: "/details/:movieId/:movieTitle/:movieOverview", */
-        path: "/details",
-        element: <DetailsPage />,
-      },
-      {
-        path: "/favorites",
-        element: <FavoritesPage />,
-      },
+      { path: "/details", element: <DetailsPage /> },
+      { path: "/favorites", element: <FavoritesPage /> },
     ],
   },
 ]);
